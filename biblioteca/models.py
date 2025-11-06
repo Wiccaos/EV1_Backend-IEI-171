45cbdfc43915eab92656b0327dd5b2b7bf9bc881
Francisca Vidal <wiccaos.moon@outlook.com>
2025-11-06 01:17:31 -0300

    feat: rut validado y edad validada (mayor de edad)

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from rut_chile import rut_chile
-from rutificador import Rut
-from django.core.exceptions import ValidationError
 import datetime
+from django.forms import ValidationError
+from rut_chile import rut_chile
 
 ahora = datetime.datetime.now
 
@@ -10,19 +9,18 @@ ahora = datetime.datetime.now
 
 
 def validar_rut(rut):
-    try:
-        rut_valido = Rut(rut)
-    except:
-        raise ValidationError('Dígito verificador NO corresponde.')
+    valido = rut_chile.is_valid_rut(rut)
+    if valido == False:
+        raise ValidationError('RUT inválido.')
 
 
 def validar_mayoria_edad(fecha_nacimiento):
-    fecha_actual = datetime.datetime.today()
+    fecha_actual = datetime.datetime.now()
     edad = fecha_actual.year - fecha_nacimiento.year
-    if (fecha_nacimiento.month, fecha_nacimiento.day) > (fecha_actual.month, fecha_actual.day):
+    if (fecha_actual.month, fecha_actual.day) < (fecha_nacimiento.month, fecha_nacimiento.day):
         edad -= 1
     if edad < 18:
-        raise ValidationError('Debe ser mayor de edad...')
+        raise ValidationError('El lector debe ser mayor de edad.')
 
 
 class Nacionalidad(models.Model):
@@ -34,6 +32,10 @@ class Nacionalidad(models.Model):
     def __str__(self):
         return self.nacionalidad
 
+class OpcionesGenero(models.TextChoices):
+    MASCULINO = 'M'
+    FEMENINO = 'F'
+
 
 class Autor(models.Model):
     id_nacionalidad = models.ForeignKey(
@@ -41,18 +43,20 @@ class Autor(models.Model):
     nombre = models.CharField(max_length=250, blank=False)
     pseudonimo = models.CharField(max_length=50, blank=True)
     biografia = models.TextField(blank=True)
+    genero = models.CharField(max_length=1,choices=OpcionesGenero.choices,default=OpcionesGenero.MASCULINO)
+    imagen_autor = models.URLField(blank=True)
     created_at = models.DateTimeField(default=ahora)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        if self.pseudonimo!='':
+        if self.pseudonimo != '':
             return self.pseudonimo
         else:
             return self.nombre
 
 
 class Comuna(models.Model):
-    codigo_comuna = models.CharField(max_length=5, blank=False)
+    codigo_comuna = models.CharField(max_length=5, blank=False, unique=True)
     nombre_comuna = models.CharField(max_length=50, blank=False)
     created_at = models.DateTimeField(default=ahora)
     updated_at = models.DateTimeField(auto_now=True)
@@ -94,14 +98,15 @@ class Lector(models.Model):
         max_length=12, blank=False, unique=True, validators=[validar_rut])
     nombre_lector = models.CharField(max_length=255, blank=False)
     correo_lector = models.CharField(max_length=255, blank=True)
-    fecha_nacimiento = models.DateField(default=datetime.date.min,
-                                        blank=True, validators=[validar_mayoria_edad])
+    fecha_nacimiento = models.DateField(
+        blank=True, default=None, validators=[validar_mayoria_edad])
+    genero = models.CharField(max_length=1,choices=OpcionesGenero.choices,default=OpcionesGenero.MASCULINO)
     habilitado = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=ahora)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.nombre_lector
+        return f'{self.nombre_lector} {self.rut_lector}'
 
 
 class TipoCategoria(models.Model):
@@ -142,6 +147,9 @@ class Libro(models.Model):
     created_at = models.DateTimeField(default=ahora)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.titulo
+
 
 class Prestamo(models.Model):
     id_libro = models.ForeignKey(Libro, on_delete=models.CASCADE, blank=False)
@@ -150,3 +158,39 @@ class Prestamo(models.Model):
     fecha_prestamo = models.DateTimeField(auto_now_add=True)
     fecha_devolucion = models.DateField(blank=True)
     fecha_retorno = models.DateTimeField(blank=True)
+
+
+class Reserva(models.Model):
+    class TipoReservaChoices(models.IntegerChoices):
+        NO_RESERVA = 0, 'Sin Reserva'
+        SEMANA = 1, '1 Semana'
+        MES = 2, '1 Mes'
+        SEMESTRE = 3, '1 Semestre'
+    id_libro = models.ForeignKey(Libro, on_delete=models.CASCADE, blank=False)
+    id_lector = models.ForeignKey(
+        Lector, on_delete=models.CASCADE, blank=False)
+    fecha_reserva = models.DateTimeField(auto_now_add=True)
+    tipo_reserva = models.IntegerField(choices=TipoReservaChoices.choices,default=TipoReservaChoices.NO_RESERVA)
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.id_lector} {self.id_libro} {self.fecha_reserva}'
+
+
+class TipoParametro(models.Model):
+    tipo_parametro = models.CharField(max_length=50, blank=False)
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Parametro(models.Model):
+    id_tipo_parametro = models.ForeignKey(
+        TipoParametro, on_delete=models.CASCADE, blank=False)
+    clave_parametro = models.CharField(max_length=100, blank=False)
+    valor_parametro = models.CharField(max_length=255, blank=True)
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
--- biblioteca/serializer.py
+++ biblioteca/serializer.py
@@ -1,221 +1,80 @@
 from rest_framework import serializers
-from .models import Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Lector, TipoCategoria, Categoria, Libro, Prestamo
+from .models import Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Lector, TipoCategoria, Categoria, Libro, Prestamo, Reserva, TipoParametro, Parametro
 
 
 class NacionalidadSerializer(serializers.ModelSerializer):
-    pais = serializers.CharField(max_length=50, label='País')
-    nacionalidad = serializers.CharField(max_length=50, label='Nacionalidad')
-
     class Meta:
         model = Nacionalidad
-        fields = [
-            'pais',
-            'nacionalidad',
-        ]
+        fields = '__all__'
 
 
 class AutorSerializer(serializers.ModelSerializer):
-    nombre = serializers.CharField(max_length=250, label='Nombre')
-    pseudonimo = serializers.CharField(max_length=50, label='Pesudónimo')
-    biografia = serializers.CharField(label='Biografía')
-
-    nacionalidad = serializers.StringRelatedField(
-        source='id_nacionalidad', read_only=True)
-    id_nacionalidad = serializers.PrimaryKeyRelatedField(
-        queryset=Nacionalidad.objects.all()
-    )
-
     class Meta:
         model = Autor
-        fields = [
-            'id',
-            'nombre',
-            'pseudonimo',
-            'biografia',
-            'id_nacionalidad',
-            'nacionalidad',
-        ]
+        fields = '__all__'
 
 
 class ComunaSerializer(serializers.ModelSerializer):
-    codigo_comuna = serializers.CharField(max_length=5, label='Código')
-    nombre_comuna = serializers.CharField(max_length=50, label='Nombre')
-
     class Meta:
         model = Comuna
-        fields = [
-            'id',
-            'codigo_comuna',
-            'nombre_comuna',
-        ]
+        fields = '__all__'
 
 
 class DireccionSerializer(serializers.ModelSerializer):
-    calle = serializers.CharField(label='Calle', max_length=50)
-    numero = serializers.CharField(label='Número', max_length=10)
-    departamento = serializers.CharField(
-        label='Departamento', max_length=10)
-    detalles = serializers.CharField(label='Detalles')
-
-    comuna = serializers.StringRelatedField(
-        source='id_comuna', read_only=True)
-    id_comuna = serializers.PrimaryKeyRelatedField(
-        queryset=Comuna.objects.all()
-    )
-
     class Meta:
         model = Direccion
-        fields = [
-            'id',
-            'calle',
-            'numero',
-            'departamento',
-            'detalles',
-            'id_comuna',
-            'comuna',
-        ]
+        fields = '__all__'
 
 
 class BibliotecaSerializer(serializers.ModelSerializer):
-    nombre_biblioteca = serializers.CharField(max_length=100, label='Nombre')
-    web = serializers.CharField(max_length=255, label='Página WEB')
-
     class Meta:
         model = Biblioteca
-        fields = [
-            'id',
-            'nombre_biblioteca',
-            'web',
-        ]
+        fields = '__all__'
 
 
 class LectorSerializer(serializers.ModelSerializer):
-    rut_lector = serializers.CharField(label='RUT')
-    nombre_lector = serializers.CharField(max_length=255, label='Nombre')
-    correo_lector = serializers.CharField(max_length=255, label='Email')
-    fecha_nacimiento = serializers.DateField(label='Fecha Nacimiento')
-
-    biblioteca = serializers.StringRelatedField(
-        source='id_biblioteca', read_only=True)
-    id_biblioteca = serializers.PrimaryKeyRelatedField(
-        queryset=Biblioteca.objects.all()
-    )
-
-    direccion = serializers.StringRelatedField(
-        source='id_direccion', read_only=True)
-    id_direccion = serializers.PrimaryKeyRelatedField(
-        queryset=Direccion.objects.all()
-    )
-
     class Meta:
         model = Lector
-        fields = [
-            'id',
-            'rut_lector',
-            'nombre_lector',
-            'correo_lector',
-            'fecha_nacimiento',
-            'id_biblioteca',
-            'biblioteca',
-            'id_direccion',
-            'direccion',
-        ]
+        fields = '__all__'
 
 
 class TipoCategoriaSerializer(serializers.ModelSerializer):
-    tipo_categoria = serializers.CharField(
-        max_length=50, label='Tipo Categoría')
-
     class Meta:
         model = TipoCategoria
-        fields = [
-            'id',
-            'tipo_categoria',
-        ]
+        fields = '__all__'
 
 
 class CategoriaSerializer(serializers.ModelSerializer):
-    categoria = serializers.CharField(max_length=100, label='Categoría')
-    descripcion = serializers.CharField(max_length=255, label='Descripción')
-
-    tipo_categoria = serializers.StringRelatedField(
-        source='id_tipo_categoria', read_only=True)
-    id_tipo_categoria = serializers.PrimaryKeyRelatedField(
-        queryset=TipoCategoria.objects.all()
-    )
-
     class Meta:
         model = Categoria
-        fields = [
-            'id',
-            'categoria',
-            'descripcion',
-            'id_tipo_categoria',
-            'tipo_categoria',
-        ]
+        fields = '__all__'
 
 
 class LibroSerializer(serializers.ModelSerializer):
-    titulo = serializers.CharField(max_length=255, label='Título')
-    paginas = serializers.IntegerField(label='Páginas')
-    copias = serializers.IntegerField(label='Copias')
-    ubicacion = serializers.CharField(max_length=255, label='Ubicación')
-    fisico = serializers.BooleanField(label='¿Es libro físico?')
-
-    biblioteca = serializers.StringRelatedField(
-        source='id_biblioteca', read_only=True)
-    id_biblioteca = serializers.PrimaryKeyRelatedField(
-        queryset=Biblioteca.objects.all()
-    )
-
-    categoria = serializers.StringRelatedField(
-        source='id_categoria', read_only=True)
-    id_categoria = serializers.PrimaryKeyRelatedField(
-        queryset=Categoria.objects.all()
-    )
-
     class Meta:
         model = Libro
-        fields = [
-            'id',
-            'titulo',
-            'paginas',
-            'copias',
-            'ubicacion',
-            'fisico',
-            'id_biblioteca',
-            'biblioteca',
-            'id_categoria',
-            'categoria',
-        ]
+        fields = '__all__'
 
 
 class PrestamoSerializer(serializers.ModelSerializer):
-    fecha_prestamo = serializers.DateTimeField(label='Fecha Préstamo')
-    fecha_devolucion = serializers.DateField(label='Fecha Devolución')
-    fecha_retorno = serializers.DateTimeField(label='Fecha Retorno')
-
-    libro = serializers.StringRelatedField(
-        source='id_libro', read_only=True)
-    id_libro = serializers.PrimaryKeyRelatedField(
-        queryset=Libro.objects.all()
-    )
-
-    lector = serializers.StringRelatedField(
-        source='id_lector', read_only=True)
-    id_lector = serializers.PrimaryKeyRelatedField(
-        queryset=Lector.objects.all()
-    )
-
     class Meta:
         model = Prestamo
-        fields = [
-            'id',
-            'fecha_prestamo',
-            'fecha_devolucion',
-            'fecha_retorno',
-            'id_libro',
-            'libro',
-            'id_lector',
-            'lector',
-        ]
+        fields = '__all__'
+
+
+class ReservaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reserva
+        fields = '__all__'
+
+
+class TipoParametroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoParametro
+        fields = '__all__'
+
+
+class ParametroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parametro
+        fields = '__all__'
